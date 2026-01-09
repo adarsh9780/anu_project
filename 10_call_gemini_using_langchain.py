d9ff0cb0f038fe8d68f65396b3ff2ae71b63cd34
Adarsh Maurya <adarshmaurya7@gmail.com>
2026-01-09 19:46:12 +0530

    added init_chat_model example

--- 10_call_gemini_using_langchain.py
+++ 10_call_gemini_using_langchain.py
@@ -1,4 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chat_models import init_chat_model
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, AnyMessage
 from dotenv import load_dotenv
 import os
@@ -11,6 +12,17 @@ model = "gemini-2.5-flash-lite"
 
 client = ChatGoogleGenerativeAI(
     model=model,
+    api_key=api_key,
+    temperature=0.7,  # Gemini 3.0+ defaults to 1.0
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+# another way to create a client
+client = init_chat_model(
+    model="google_genai:gemini-2.5-flash-lite",  # or gpt-4.1, claude-sonnet-4-5-20250929
     api_key=api_key,
     temperature=0.7,  # Gemini 3.0+ defaults to 1.0
     max_tokens=None,
